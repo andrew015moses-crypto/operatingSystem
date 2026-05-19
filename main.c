2b7e19db864700ce703a19e1461e17141c84d99f
andrew015moses-crypto <andrew015moses@gmail.com>
2026-05-18 23:05:57 -0700

    Add files via upload
    here we two functions to create a process and want it to store it into the process table and execute process and we have use pointer inside as parameter to have direct access to the actual parent process and const key word is used because we don't want to modify name anyhow

--- main.c
+++ main.c
@@ -45,3 +45,51 @@ void print_process_table(void);
 ProcessControlBlock process_table[MAX_PROCESSES];
 int next_process_id = 1;
 int total_processes = 0;
+
+void create_process(ProcessControlBlock *parent_process, const char *program_name)
+{
+    if (total_processes >= MAX_PROCESSES)
+    {
+        printf("Process table is full.\n");
+        return;
+    }
+
+    ProcessControlBlock *new_process = &process_table[total_processes];
+    new_process->process_id = next_process_id++;
+    new_process->parent_process_id = parent_process ? parent_process->process_id : 0;
+    strcpy(new_process->program_name, program_name);
+    new_process->current_state = READY;
+    new_process->priority_level = 1;    
+    new_process->arrival_time = 0;
+    new_process->burst_time = 10;       
+    new_process->remaining_time = 10;
+    new_process->memory_required_kb = 1024;
+    new_process->creation_timestamp = time(NULL);
+    new_process->thread_count = 1;
+    new_process->exit_code = 0;
+
+    total_processes++;
+
+    printf("[%ld] Process created | PID=%d | Program=%s\n",
+           (long)time(NULL), new_process->process_id, new_process->program_name);
+}
+
+void execute_process(int process_id, const char *new_program_name)
+{
+    for (int i = 0; i < total_processes; ++i)
+    {
+        if (process_table[i].process_id == process_id)
+        {
+            process_table[i].current_state = RUNNING;
+            strcpy(process_table[i].program_name, new_program_name);
+            printf("[%ld] Executing PID=%d -> %s\n",
+                   (long)time(NULL), process_id, process_table[i].program_name);
+            return;
+        }
+    }
+
+    printf("Process %d not found.\n", process_id);
+}
+
+
+ 
